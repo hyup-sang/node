df61dc69233c8dd5e6b8470b57859112e595db60
SH <rtg1539@naver.com>
2023-09-09 17:34:34 +0900

    readable

--- stream/readable/index.js
+++ stream/readable/index.js
@@ -1,10 +1,15 @@
 import { RandomStream } from "./readable-stream.js";
 
 const randomStream = new RandomStream();
-randomStream
-  .on("data", (chunk) => {
-    console.log(`Chunk received (${chunk.length} bytes): ${chunk.toString()}`);
-  })
-  .on("end", () => {
-    console.log(`Produced ${randomStream.emittedBytes} bytes of random data`);
-  });
+// randomStream
+//   .on("data", (chunk) => {
+//     console.log(`Chunk received (${chunk.length} bytes): ${chunk.toString()}`);
+//   })
+//   .on("end", () => {
+//     console.log(`Produced ${randomStream.emittedBytes} bytes of random data`);
+//   });
+
+for await (const chunk of randomStream) {
+  console.log(`Chunk received (${chunk.length} bytes): ${chunk.toString()}`);
+}
+console.log(`Produced ${randomStream.emittedBytes} bytes of random data`);
