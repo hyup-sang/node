0663003ac6268b2adfe1966027ac74fac7440ae3
SH <rtg1539@naver.com>
2023-10-05 22:41:19 +0900

    sequential

--- 06-stream/17-pipeline-helper/uppercasify-gzipped-promise.js
+++ 06-stream/17-pipeline-helper/uppercasify-gzipped-promise.js
@@ -1,26 +1,29 @@
-import { Transform } from 'stream'
+import { createGzip, createGunzip } from 'zlib'
+import { Transform, pipeline } from 'stream'
+import { promisify } from 'util'
 
-export class ReplaceStream extends Transform {
-  constructor (searchStr, replaceStr, options) {
-    super({ ...options })
-    this.searchStr = searchStr
-    this.replaceStr = replaceStr
-    this.tail = ''
+const pipelinePromise = promisify(pipeline)
+
+const uppercasify = new Transform({
+  transform (chunk, enc, cb) {
+    this.push(chunk.toString().toUpperCase())
+    cb()
   }
+})
 
-  _transform (chunk, encoding, callback) {
-    const pieces = (this.tail + chunk).split(this.searchStr)
-    const lastPiece = pieces[pieces.length - 1]
-    const tailLen = this.searchStr.length - 1
-    this.tail = lastPiece.slice(-tailLen)
-    pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen)
-
-    this.push(pieces.join(this.replaceStr))
-    callback()
-  }
-
-  _flush (callback) {
-    this.push(this.tail)
-    callback()
+async function main () {
+  try {
+    await pipelinePromise(
+      process.stdin,
+      createGunzip(),
+      uppercasify,
+      createGzip(),
+      process.stdout
+    )
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
   }
 }
+
+main()
